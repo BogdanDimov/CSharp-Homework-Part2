dba20301351b8738875d61e1bfab5a27402ca030
BogdanDimov <bogdan.dimov@outlook.com>
2016-11-02 22:26:51 +0200

    update

---
+++ TrailingZeroInN!/TrailingZeroInN!.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TrailingZeroInN_
+{
+    class Program
+    {
+        static void Main()
+        {
+            uint N = uint.Parse(Console.ReadLine());
+
+            uint count = 0; // number of trailing zeros
+
+            for (uint i = 5; i <= N; i *= 5)
+            {
+                count += N / i;
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
