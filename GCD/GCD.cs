711cb73cf7860e8e08c4267df1c82dd1d14eb1b5
BogdanDimov <bogdan.dimov@outlook.com>
2016-11-02 20:42:49 +0200

    update

---
+++ GCD/GCD.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace GCD
+{
+    class GCD
+    {
+        static void Main()
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int A = Math.Abs(int.Parse(input[0]));
+            int B = Math.Abs(int.Parse(input[1]));
+
+            int temp = A;
+
+            while (A != 0 && B != 0)
+            {
+                if (A > B)
+                {
+                    temp = B;
+                    B = A % B;
+                    A = temp;
+                }
+                else
+                {
+                    temp = A;
+                    A = B % A;
+                    B = temp;
+                }
+            }
+
+            Console.WriteLine(A == 0 ? B : A);
+        }
+    }
+}
