cd3be8efe37ccb720f3961e33757c34c820b5bea
BogdanDimov <bogdan.dimov@outlook.com>
2016-11-08 16:58:16 +0200

    update

---
+++ CompareCharArrays/CompareCharArrays.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CompareCharArrays
+{
+    class CompareCharArrays
+    {
+        static void Main()
+        {
+            string s1 = Console.ReadLine(); string s2 = Console.ReadLine();
+
+            string result = "=";
+
+            int length = Math.Min(s1.Length, s2.Length);
+
+            for (int i = 0; i < length; i++)
+            {
+                if (s1[i] < s2[i])
+                {
+                    result = "<"; break;
+                }
+                else if (s1[i] > s2[i])
+                {
+                    result = ">"; break;
+                }
+                else
+                {
+                    if (s1.Length > s2.Length)
+                    {
+                        result = ">";
+                    }
+                    else if (s1.Length < s2.Length)
+                    {
+                        result = "<";
+                    }
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ MergeSort/MergeSort.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSort
+{
+    class MergeSort
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] a, b; a = b = new int[n];
+
+            for (int id = 0; id < n; id++)
+            {
+                a[id] = int.Parse(Console.ReadLine()); ;
+            }
+
+            int i, j;
+            int iMiddle = n / 2;
+            i = 0; j = iMiddle;
+
+            for (int k = 0; k < n; k++)
+            {
+                if (k < iMiddle && (j >= n || a[i] <= a[j]))
+                {
+                    b[k] = a[i];
+                    i++;
+                }
+                else
+                {
+                    b[k] = a[j];
+                    j++;
+                }
+            }
+
+            Console.WriteLine(string.Join("\n", b));
+        }
+    }
+}
